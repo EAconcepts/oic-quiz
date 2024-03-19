496bc29ffa85a8600e0340f8ab8b1e49809fe946
Emmanuel <hahyordeji@gmail.com>
2024-03-19 10:45:04 +0100

    Added ms powerpoint questions

---
+++ powerpoint.js
@@ -0,0 +1,210 @@
+const powerpoint_questions = [
+  {
+    question: "What file extension is used for PowerPoint presentations?",
+    options: ["*.pptx", "*.ppt", "*.pps", "*.potx"],
+    answer: "*.pptx",
+  },
+  {
+    question:
+      "Which view in PowerPoint allows you to see multiple slides at once?",
+    options: [
+      "Normal View",
+      "Slide Show View",
+      "Slide Sorter View",
+      "Outline View",
+    ],
+    answer: "Slide Sorter View",
+  },
+  //   {
+  //     question: "What is the purpose of 'Design ribbon' feature in PowerPoint?",
+  //     options: [
+  //       "To create animations",
+  //       "To suggest design layouts for slides",
+  //       "To insert charts and graphs",
+  //       "To spell check the presentation",
+  //     ],
+  //     answer: "To suggest design layouts for slides",
+  //   },
+  // {
+  //     "question": "Which feature in PowerPoint allows you to rehearse your presentation timings?",
+  //     "options": ["Presenter View", "Slide Sorter View", "Record Slide Show", "Speaker Notes"],
+  //     "answer": "Record Slide Show"
+  // },
+  {
+    question: "What is the function of the 'Transitions' tab in PowerPoint?",
+    options: [
+      "To insert audio files",
+      "To add comments to slides",
+      "To insert hyperlinks",
+      "To apply animation effects between slides",
+    ],
+    answer: "To apply animation effects between slides",
+  },
+  {
+    question:
+      "Which feature in PowerPoint allows you to record narration for your presentation?",
+    options: [
+      "Record Slide Show",
+      "Presenter View",
+      "Speaker Notes",
+      "Slide Show View",
+    ],
+    answer: "Record Slide Show",
+  },
+  {
+    question: "What is the purpose of 'Slide Master' in PowerPoint?",
+    options: [
+      "To add animations to slides",
+      "To insert audio files",
+      "To set the overall layout and formatting of slides",
+      "To create hyperlinks",
+    ],
+    answer: "To set the overall layout and formatting of slides",
+  },
+//   {
+//     question:
+//       "Which option allows you to print handouts with multiple slides per page?",
+//     options: [
+//       "Handout Master",
+//       "Presenter View",
+//       "Slide Sorter View",
+//       "Outline View",
+//     ],
+//     answer: "Handout Master",
+//   },
+  {
+    question: "What is the purpose of 'Animation Pane' in PowerPoint?",
+    options: [
+      "To insert videos into slides",
+      "To apply design templates",
+      "To record narration",
+      "To control the timing and order of animations",
+    ],
+    answer: "To control the timing and order of animations",
+  },
+  // {
+  //     "question": "Which option in PowerPoint allows you to embed fonts in your presentation?",
+  //     "options": ["Save As", "Slide Master", "Design Ideas", "Custom Animation"],
+  //     "answer": "Save As"
+  // },
+  {
+    question: "What is the default slide layout in PowerPoint?",
+    options: ["Title and Content", "Two Content", "Blank", "Title Slide"],
+    answer: "Title Slide",
+  },
+  {
+    question:
+      "Which ribbon in PowerPoint allows you to add motion paths to objects?",
+    options: ["Insert", "Design ", "Transitions", "Animation"],
+    answer: "Animation",
+  },
+  {
+    question: "What does the 'Rehearse Timings' option in PowerPoint do?",
+    options: [
+      "Records the time spent on each slide",
+      "Checks for spelling errors",
+      "Adds animation effects to slides",
+      "Records narration",
+    ],
+    answer: "Records the time spent on each slide",
+  },
+  {
+    question:
+      "Which view in PowerPoint allows you to see the structure of your presentation?",
+    options: [
+      "Normal View",
+      "Outline View",
+      "Slide Sorter View",
+      "Presenter View",
+    ],
+    answer: "Outline View",
+  },
+  {
+    question: "What is the function of 'Slide Show' tab in PowerPoint?",
+    options: [
+      "To apply design templates",
+      "To insert audio files",
+      "To start the presentation",
+      "To add comments to slides",
+    ],
+    answer: "To start the presentation",
+  },
+  {
+    question: "What does 'Slide Transition' in PowerPoint refer to?",
+    options: [
+      "The animation of text on a slide",
+      "The movement between slides",
+      "The transition between PowerPoint versions",
+      "The addition of a new slide",
+    ],
+    answer: "The movement between slides",
+  },
+  {
+    question: "Which of the following is NOT a PowerPoint animation effect?",
+    options: ["Fade", "Spin", "Flip", "Zoom"],
+    answer: "Flip",
+  },
+  // {
+  //     "question": "Which feature in PowerPoint allows you to create custom animations?",
+  //     "options": ["Custom Animation", "Design Ideas", "Transitions", "Animation Pane"],
+  //     "answer": "Custom Animation"
+  // },
+  {
+    question: "Which is the purpose of 'Presenter View' in PowerPoint?",
+    options: [
+      "To add animations to slides",
+      "To apply design templates",
+      "To insert audio files",
+      "To view presenter notes while presenting",
+    ],
+    answer: "To view presenter notes while presenting",
+  },
+  {
+    question:
+      "Which Ribbon in PowerPoint allows you to check for spelling errors?",
+    options: ["Design", "Animation", "Review Tab", "Slide Sorter View"],
+    answer: "Review Tab",
+  },
+  {
+    question: "What is the function of 'SmartArt' in PowerPoint?",
+    options: [
+      "To apply design templates",
+      "To insert audio files",
+      "To create visual representations of information",
+      "To add comments to slides",
+    ],
+    answer: "To create visual representations of information",
+  },
+  {
+    question:
+      "Which ribbon in PowerPoint allows you to insert audio or video files?",
+    options: ["Insert Tab", "Design", "Transitions", "Home "],
+    answer: "Insert Tab",
+  },
+  //   {
+  //     question: "What is the purpose of 'Speaker Notes' in PowerPoint?",
+  //     options: [
+  //       "To add notes visible only to the presenter",
+  //       "To record narration",
+  //       "To apply design templates",
+  //       "To insert audio files",
+  //     ],
+  //     answer: "To add notes visible only to the presenter",
+  //   },
+  {
+    question: "What is the default slide orientation in PowerPoint?",
+    options: ["Portrait", "Landscape", "Square", "A4"],
+    answer: "Landscape",
+  },
+  {
+    question: "Which command allows you to duplicate a slide in PowerPoint?",
+    options: ["Ctrl + C", "Ctrl + D", "Ctrl + V", "Ctrl + X"],
+    answer: "Ctrl + D",
+  },
+  {
+    question:
+      "What is the shortcut key to start a slideshow from the beginning in PowerPoint?",
+    options: ["F7", "F5", "F9", "F12"],
+    answer: "F5",
+  },
+];
